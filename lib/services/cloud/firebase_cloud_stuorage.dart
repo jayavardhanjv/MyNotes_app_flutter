35b24f5439e8c823f6b6c0f5e2083fd944f2eaea
unknown <jjayavardhan81@gmail.com>
2023-07-23 13:17:52 +0530

    step 19

---
+++ lib/services/cloud/firebase_cloud_stuorage.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_2/services/cloud/cloud_note.dart';
+import 'package:flutter_application_2/services/cloud/cloud_storage_constants.dart';
+import 'package:flutter_application_2/services/cloud/cloud_storage_exceptions.dart';
+//this file is used to do curd in firestore 
+class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection('notes');
+//here we are delete=ing note of the user in firestore
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      notes.doc(documentId).delete()
+    } catch (e) {
+      throw CouldNotDeleteNoteException();
+    }
+  }
+//here we are updating note of the user in firestore
+
+  Future<void> updateNote({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      notes.doc(documentId).update({textFieldName: text});
+    } catch (e) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
+
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then(
+            (value) => value.docs.map(
+              (doc) {
+                return CloudNote(
+                  documentId: doc.id,
+                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                  text: doc.data()[textFieldName] as String,
+                );
+              },
+            ),
+          );
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
+  void creteNewNote({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+  factory FirebaseCloudStorage() => _shared;
+}
